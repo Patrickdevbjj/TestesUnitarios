d774673e3a98bb579ffaeba555040ed93dc50445
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-02-18 17:01:05 -0300

    more exemples

--- src/test/java/AssertTest.java
+++ src/test/java/AssertTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.ce.wcaquino.entidades.Usuario;
+
 
 
 public class AssertTest {
@@ -13,5 +15,21 @@ public class AssertTest {
 		//serve para trabalhar com inteiro, short, long etc, porem se comporta diferente com float e double
 		Assert.assertEquals(2, 2);
 		Assert.assertEquals(0.51, 1.51, 1); //margem de erro 
+		
+		int i = 5;
+		Integer i2 = 5;
+		Assert.assertEquals(Integer.valueOf(i), i2);	
+		Assert.assertEquals(i, i2.intValue());
+		
+		Assert.assertEquals("bola", "bola");
+		Assert.assertTrue("Bola".equalsIgnoreCase("bola"));
+		Assert.assertTrue("bola".startsWith("bo"));
+
+		Usuario u1 = new Usuario("Usuario 1");
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = new Usuario(null);
+		
+		Assert.assertEquals(u1, u2);
+		
 	}
 }
